f5e54948b7f290b177e9d14ec080237fd04ae1b4
cavalcante <educavalcantedev@gmail.com>
2023-12-28 13:23:55 -0300

    Implementation of discovery service with Eureka

--- amigosservices/customer/src/main/java/com/company/customer/CustomerService.java
+++ amigosservices/customer/src/main/java/com/company/customer/CustomerService.java
@@ -23,7 +23,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
         // todo: check if fraudster
         FraudCheckHistoryResponse fraudCheckHistoryResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://fraud/api/v1/fraud-check/{customerId}",
                 FraudCheckHistoryResponse.class,
                 customer.getId()
         );
