fa041c524b8be90c2981cead487ec04b6db3545b
cavalcante <educavalcantedev@gmail.com>
2024-01-07 20:20:13 -0300

    Refactoring Microservices for k8s

--- amigosservices/notification/src/main/java/com/company/notification/NotificationApplication.java
+++ amigosservices/notification/src/main/java/com/company/notification/NotificationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -13,6 +15,9 @@ import org.springframework.context.annotation.Bean;
                 "com.company.amqp"
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
